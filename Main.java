b7756cb08ff46f4b4d9bef845a12a64f0ad720cf
MichelledeGroot <31917104+MichelledeGroot@users.noreply.github.com>
2018-09-20 11:24:21 +0200

    deel 1

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student stud1 = new Student("Sophie", 20,581613 );
+        Student stud2 = new Student("Teun",20);
+        Student stud3 = new Student("Valerie", 19);
+        Student stud4 = new Student("Anne", 19);
+        Student stud5 = new Student("Bram");
+
+        stud1.info();
+        stud2.info();
+        stud5.info();
+    }
+
+}
